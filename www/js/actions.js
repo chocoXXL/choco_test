27c277b9d9acff3d3f7f1277a284c420d090aed0
chocoXXL <onecoldplayer@hotmail.com>
2016-09-23 20:15:04 -0500

    jojo

--- www/js/actions.js
+++ www/js/actions.js
@@ -5,56 +5,85 @@ $(document).ready(function() {
 
 $( "#nav_btn" ).click(function() {
 if ($( "#nav_main" ).hasClass( "nav_show" )) {
-	console.log("osea hello NO");
-	$("#nav_main").addClass("nav_hide");
-	$("#nav_main").removeClass("nav_show");
-	}
+    console.log("osea hello NO");
+    $("#nav_main").addClass("nav_hide");
+    $("#nav_main").removeClass("nav_show");
+    }
 else {
-	console.log("osea hello");
-	$("#nav_main").removeClass("nav_hide");
-	$("#nav_main").addClass("nav_show");
-	}
+    console.log("osea hello");
+    $("#nav_main").removeClass("nav_hide");
+    $("#nav_main").addClass("nav_show");
+    }
 });
 
 
 /* LA BURGER */
-    var bittee = 1;	
-    $( ".btn_mordida, .nav_li_btn " ).click(function() {
+    var bittee = 1; 
+    $( ".btn_mordida" ).click(function() {
+
         console.log('bite bite')
-        if (bittee == 1) { 
-            $(".burger").addClass("bite");
-            $("#nav_main").addClass("nav_show");
-            $("#btn_mordida").addClass("bite");
-            bittee = 2;
-            console.log('are SI');
-        }
-        else {
-            $(".burger").removeClass("bite");
-            $("#nav_main").addClass("nav_hide");
-			$("#nav_main").removeClass("nav_show");
-			$("#btn_mordida").removeClass("bite");
-            bittee = 1;
-            console.log('are NO');
-        }
+
+            if (bittee == 1) { 
+                $(".burger").addClass("bite");
+                $("#nav_main").addClass("nav_show");
+                $("#btn_mordida").addClass("bite");
+                bittee = 2;
+                console.log('are SI');
+            }
+            else {
+                $(".burger").removeClass("bite");
+                $("#nav_main").addClass("nav_hide");
+                $("#nav_main").removeClass("nav_show");
+                $("#btn_mordida").removeClass("bite");
+                bittee = 1;
+                console.log('are NO');
+            }
+
     });
 
+    $( ".nav_li_btn " ).click(function() {
+
+       console.log("espera...");
+            setTimeout(
+            function() {
+                       
+                    if (bittee == 1) { 
+                $(".burger").addClass("bite");
+                $("#nav_main").addClass("nav_show");
+                $("#btn_mordida").addClass("bite");
+                bittee = 2;
+                console.log('are SI');
+                    }
+                    else {
+                        $(".burger").removeClass("bite");
+                        $("#nav_main").addClass("nav_hide");
+                        $("#nav_main").removeClass("nav_show");
+                        $("#btn_mordida").removeClass("bite");
+                        bittee = 1;
+                        console.log('are NO');
+                    }
+            },
+            400);
+    });
 
 /* EL SCROLL CROLL*/
 $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-            $('html, body').animate({
-              //scrollTop: target.offset().top
-               scrollTop: target.offset().top - 0
-            }, 1000);
-            return false;
-            }
-        }
-    });
+                 if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                        if (target.length) {
+                        $('html, body').animate({
+                          //scrollTop: target.offset().top
+                           scrollTop: target.offset().top - 0
+                        }, 2000);
+                        return false;
+                        }
+                    }
+     });
 });
+
+
 
 });
 
@@ -75,5 +104,5 @@ function onFail(message){
     alert(message);
 }//FIN
 
-//	$("nav").toggle("slow", function(){
-//	});
+//  $("nav").toggle("slow", function(){
+//  });
